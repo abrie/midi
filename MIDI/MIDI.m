f6051f0a46c88ec7b7bbe756e20497c84d6027e2
abrie <abrie@errante.local>
2012-09-22 11:40:16 -0400

    refactor: generalize common transmit methods into helper method. The two cases are: transmitting only a 1 byte status, or a full 3 byte (status+2 data).

--- MIDI/MIDI.m
+++ MIDI/MIDI.m
@@ -155,84 +155,72 @@ static void midiRead(const MIDIPacketList *pktlist, void *readProcRefCon, void *
 }
 
 - (void)transmitToEndpoint:(MIDIEndpointRef)endpoint
-                      byte:(unsigned int)byte
+                   message:(Byte[])message
+                    length:(unsigned int)length
 {
     Byte buffer[1024];
 	MIDIPacketList *packet_list = (MIDIPacketList *)buffer;
 	MIDIPacket *packet = MIDIPacketListInit(packet_list);
-	Byte message[1];
+	
 	OSStatus result;
     
-	message[0] = byte;
-	
-    packet = MIDIPacketListAdd(
+	packet = MIDIPacketListAdd(
                                packet_list,
                                sizeof(buffer),
                                packet,
                                0,
-                               1,
+                               length,
                                message);
 	
     result = MIDISend(output_port, endpoint, packet_list);
 }
 
 - (void)transmitToEndpoint:(MIDIEndpointRef)endpoint
-                    status:(unsigned)byte_1
-                    data_1:(unsigned)byte_2
-                    data_2:(unsigned)byte_3
+                      status:(Byte)status
 {
-	Byte buffer[1024];
-	MIDIPacketList *packet_list = (MIDIPacketList *)buffer;
-	MIDIPacket *packet = MIDIPacketListInit(packet_list);
-	Byte message[3];
-	OSStatus result;
-    
-	message[0] = byte_1;
-	message[1] = byte_2;
-	message[2] = byte_3;
-	
-	packet = MIDIPacketListAdd(packet_list,
-                               sizeof(buffer),
-                               packet,
-                               0,
-                               3,
-                               message);
-    
-	result = MIDISend(output_port,
-                      endpoint,
-                      packet_list);
+    Byte message[1] = {status};
+    [self transmitToEndpoint:endpoint message:message length:1];
+}
+
+- (void)transmitToEndpoint:(MIDIEndpointRef)endpoint
+                    status:(Byte)status
+                    data1:(Byte)data1
+                    data2:(Byte)data2
+{
+	Byte message[3] = {status, data1, data2};
+	[self transmitToEndpoint:endpoint message:message length:3];
 }
 
 - (void)sendSongPosition:(unsigned int)position
 {
-    Byte byte1 = position & 0xFF;
-    Byte byte2 = position >> 8;
-    [self transmitToEndpoint:out_endpoint status:0xF2 data_1:byte1 data_2:byte2];
+    Byte data1 = position & 0xFF;
+    Byte data2 = position >> 8;
+    [self transmitToEndpoint:out_endpoint status:0xF2 data1:data1 data2:data2];
 }
 
 - (void)sendClock
 {
-    [self transmitToEndpoint:out_endpoint byte:0xF8];
+    [self transmitToEndpoint:out_endpoint status:0xF8];
 }
 
 - (void)sendTick
 {
-    [self transmitToEndpoint:out_endpoint byte:0xF9];
+    [self transmitToEndpoint:out_endpoint status:0xF9];
 }
 
 - (void)sendStart
 {
-    [self transmitToEndpoint:out_endpoint byte:0xFA];
+    [self transmitToEndpoint:out_endpoint status:0xFA];
 }
 
 - (void)sendContinue
 {
-    [self transmitToEndpoint:out_endpoint byte:0xFB];
+    [self transmitToEndpoint:out_endpoint status:0xFB];
 }
 
 - (void)sendStop
 {
-    [self transmitToEndpoint:out_endpoint byte:0xFC];
+    [self transmitToEndpoint:out_endpoint status:0xFC];
 }
 
 - (void)sendOnToChannel:(unsigned int)channel
@@ -241,8 +229,8 @@ static void midiRead(const MIDIPacketList *pktlist, void *readProcRefCon, void *
 {
 	[self transmitToEndpoint:out_endpoint
                       status:0x90 + channel
-                      data_1:number
-                      data_2:velocity];
+                      data1:number
+                      data2:velocity];
 }
 
 - (void)sendOffToChannel:(unsigned int)channel
@@ -251,8 +239,8 @@ static void midiRead(const MIDIPacketList *pktlist, void *readProcRefCon, void *
 {
 	[self transmitToEndpoint:out_endpoint
                       status:0x80 + channel
-                      data_1:number
-                      data_2:velocity ];
+                      data1:number
+                      data2:velocity ];
 }
 
 - (void)notify_midiClock
